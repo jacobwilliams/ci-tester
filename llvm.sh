b9d9d9a47a68526c4277cbd8728a455ef4cc5fd9
Jacob Williams <jacobwilliams@users.noreply.github.com>
2024-05-20 22:12:12 -0500

    rm -dev

--- llvm.sh
+++ llvm.sh
@@ -171,9 +171,13 @@ PKG="clang-$LLVM_VERSION lldb-$LLVM_VERSION lld-$LLVM_VERSION clangd-$LLVM_VERSI
 if [[ $ALL -eq 1 ]]; then
     # same as in test-install.sh
     # No worries if we have dups
-    PKG="$PKG clang-tidy-$LLVM_VERSION clang-format-$LLVM_VERSION clang-tools-$LLVM_VERSION llvm-$LLVM_VERSION-dev lld-$LLVM_VERSION lldb-$LLVM_VERSION llvm-$LLVM_VERSION-tools libomp-$LLVM_VERSION-dev libc++-$LLVM_VERSION-dev libc++abi-$LLVM_VERSION-dev libclang-common-$LLVM_VERSION-dev libclang-$LLVM_VERSION-dev libclang-cpp$LLVM_VERSION-dev libunwind-$LLVM_VERSION-dev flang-$LLVM_VERSION-dev"
+    # PKG="$PKG clang-tidy-$LLVM_VERSION clang-format-$LLVM_VERSION clang-tools-$LLVM_VERSION llvm-$LLVM_VERSION-dev lld-$LLVM_VERSION lldb-$LLVM_VERSION llvm-$LLVM_VERSION-tools libomp-$LLVM_VERSION-dev libc++-$LLVM_VERSION-dev libc++abi-$LLVM_VERSION-dev libclang-common-$LLVM_VERSION-dev libclang-$LLVM_VERSION-dev libclang-cpp$LLVM_VERSION-dev libunwind-$LLVM_VERSION-dev flang-$LLVM_VERSION-dev"
+    # if test $LLVM_VERSION -gt 14; then
+    #     PKG="$PKG libclang-rt-$LLVM_VERSION-dev libpolly-$LLVM_VERSION-dev"
+    # fi
+    PKG="$PKG clang-tidy-$LLVM_VERSION clang-format-$LLVM_VERSION clang-tools-$LLVM_VERSION llvm-$LLVM_VERSION lld-$LLVM_VERSION lldb-$LLVM_VERSION llvm-$LLVM_VERSION-tools libomp-$LLVM_VERSION libc++-$LLVM_VERSION libc++abi-$LLVM_VERSION libclang-common-$LLVM_VERSION libclang-$LLVM_VERSION libclang-cpp$LLVM_VERSION libunwind-$LLVM_VERSION flang-$LLVM_VERSION"
     if test $LLVM_VERSION -gt 14; then
-        PKG="$PKG libclang-rt-$LLVM_VERSION-dev libpolly-$LLVM_VERSION-dev"
+        PKG="$PKG libclang-rt-$LLVM_VERSION libpolly-$LLVM_VERSION"
     fi
 fi
 apt-get install -y $PKG
